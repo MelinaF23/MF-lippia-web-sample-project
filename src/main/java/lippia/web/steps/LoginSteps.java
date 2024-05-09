ec523ed5e4ee45c969b7d4b03a8b265d681eb2ee
MelinaF23 <melina.fittipaldi@crowdaronline.com>
2024-05-08 21:50:29 -0300

    Nuevo Service

--- src/main/java/lippia/web/steps/LoginSteps.java
+++ src/main/java/lippia/web/steps/LoginSteps.java
@@ -6,21 +6,24 @@ import lippia.web.services.LoginService;
 
 public class LoginSteps extends PageSteps {
 
-    //LoginService loginService = new LoginService();
+    //LoginService loginService = new LoginService(); INFO DE UNA CONSULTA
     @And("hace click en Log in manually")
     public void haceClickEnLogInManually() {
         LoginService.click();
     }
 
     @And("coloca un email válido {string}")
-    public void colocaUnEmailVálido(String email) {LoginService.enterEmail(email);
+    public void colocaUnEmailVálido(String email) {
+        LoginService.enterEmail(email); //Aca podria haber sido setEmail
     }
 
     @And("ingresa la contraseña válida {string}")
-    public void ingresaLaContraseñaVálida(String password) {LoginService.enterPassword(password);
+    public void ingresaLaContraseñaVálida(String password) {
+        LoginService.enterPassword(password);
     }
 
     @And("hace click en Log in en la pantalla de Login")
-    public void haceClickEnLogInEnLaPantallaDeLogin() { LoginService.clickLogin();
+    public void haceClickEnLogInEnLaPantallaDeLogin() {
+        LoginService.clickLogin();
     }
 }
